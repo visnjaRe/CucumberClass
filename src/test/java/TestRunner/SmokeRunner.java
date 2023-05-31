2b4d674585d0124d5aac6db7ddaa8e6eff322013
[VisnjaRepac] <visnja.repac@gmail.com>
2023-05-31 18:26:39 -0500

    second commit

--- src/test/java/TestRunner/SmokeRunner.java
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -3,11 +3,22 @@ package TestRunner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = "StepDefinition", dryRun = true)
+@CucumberOptions(features = "src/test//resources/Features",
+        glue = "StepDefinitions",
+        dryRun = false,
+        tags = "@testcase2",
+        plugin = {"pretty", "html:target/Cucumber.html", "json:target/Cucumber.json", "rerun:target/failed.txt"}
+
+)
 
 public class SmokeRunner {
 }
+
+
+//tags option will execute the tagged testcase as mentioned in your runner class
+//target folder is mostly used for storing the test case execution reports generated using Cucumber
 
 //if we wanna run certain feature we have to use @ in .feature class above scenario
 //tags option will execute the tagged testcase as mentioned in your runner class
--- src/test/java/Utils/CommonMethods.java
+++ src/test/java/Utils/CommonMethods.java
@@ -1,15 +1,27 @@
 package Utils;
-
+import StepDefinition.PageInitializer;
+import StepDefinition.PageInitializer;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
+import java.util.List;
 
-public class CommonMethods {
+public class CommonMethods extends PageInitializer {
     public static WebDriver driver;
 
     public static void openBrowserAndLaunchApplication() {
@@ -18,7 +30,13 @@ public class CommonMethods {
         String browserType = ConfigReader.getPropertyValue("browserType");
         switch (browserType) {
             case "Chrome":
-                driver = new ChromeDriver();
+                ChromeOptions ops = new ChromeOptions();
+                ops.addArguments("--no-sandbox");
+                ops.addArguments("--remote-allow-origins=*");
+                if(ConfigReader.getPropertyValue("Headless").equals("true")){
+                    ops.addArguments("--headless=new");
+                }
+                driver = new ChromeDriver(ops);
                 break;
 
             case "Firefox":
@@ -38,10 +56,20 @@ public class CommonMethods {
         driver.manage().window().maximize();
         driver.get(ConfigReader.getPropertyValue("url"));
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(Constants.WAIT_TIME));
+        initializePageObjects(); // This will initialize all the pages we have in our Page
+        //PageInitializer class along with the launching of application
+        //to configure the file and the pattern it has
+        DOMConfigurator.configure("log4j.xml");
+        Log.startTestCase("This is the begging of my Test Case");
+        Log.info("My test case is executing now");
+        Log.warning("My test case might have some trivial issues");
+
     }
 
 
     public static void closeBrowser() {
+        Log.info("this is tx about to get completed");
+        Log.endTestCase("TC is finished");
         driver.close();
     }
 
@@ -53,6 +81,62 @@ public class CommonMethods {
     public static void sendText(WebElement element, String text) {
         element.clear();
         element.sendKeys(text);
+    }
+
+    public static Select clickOnDropdown(WebElement element) {
+        Select select = new Select(element);
+        return select;
+    }
+
+    public static void selectByValue(WebElement element, String value) {
+        clickOnDropdown(element).selectByValue(value);
+    }
+
+    public static void selectByVisibleText(WebElement element, String text) {
+        clickOnDropdown(element).selectByVisibleText(text);
+    }
+
+    public static void selectByIndex(WebElement element, int index) {
+        clickOnDropdown(element).selectByIndex(index);
+    }
+
+    public static void selectByOptions(WebElement element, String text) {
+        List<WebElement> options = clickOnDropdown(element).getOptions();
+        for (WebElement option : options) {
+            String ddlOptionText = option.getText();
+            if (ddlOptionText.equals(text)) {
+                option.click();
+            }
+        }
+    }
+
+    //========================SCREENSHOT ===============================
+
+    public static byte[] takeScreenshot(String imageName)
+    {
+        // This casts the webDriver instance 'driver' to TakeScreenshot Interface
+        TakesScreenshot ts = (TakesScreenshot)driver;
+
+        //This captures the screenshot and stores it as byte array
+        byte[] picBytes=ts.getScreenshotAs(OutputType.BYTES);
+
+        //This captures the screenshot and stores it as a file in the sourceFile variable
+        File sourcePath=ts.getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(sourcePath, new File(Constants.SCREENSHOT_FILEPATH+imageName+getTimeStamp("yyyy-MM-dd-HH-mm-ss")+".png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return picBytes;
+    }
+
+
+    public  static  String getTimeStamp(String pattern)
+    {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        return sdf.format(date);
     }
 
 }
